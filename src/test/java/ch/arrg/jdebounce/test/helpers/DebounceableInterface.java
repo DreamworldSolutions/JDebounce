920175c857034174eb69977d6c9f3552bc3a9f8f
Jaydeep Kumbhani <jaydeep.kumbhani@dreamworldsolutions.com>
2017-08-22 17:43:16 +0530

    removed unused and error code

--- src/test/java/ch/arrg/jdebounce/test/helpers/DebounceableInterface.java
+++
@@ -1,9 +0,0 @@
-package ch.arrg.jdebounce.test.helpers;
-
-/**
- * When using Spring-aop, it is mandatory that the debounced methods correspond
- * to an interface. Otherwise proxying won't work.
- */
-public interface DebounceableInterface {
-	public void debouncedMethod(int callID, DebounceTest callback);
-}
